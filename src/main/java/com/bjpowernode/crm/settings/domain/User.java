597145acc3a9613703df6fd89a253d1a88f90fdb
zs <za@bjpowernode.com>
2021-04-20 12:14:16 +0900

    4.20am

---
+++ src/main/java/com/bjpowernode/crm/settings/domain/User.java
@@ -0,0 +1,18 @@
+package com.bjpowernode.crm.settings.domain;
+
+public class User {
+
+    private String id;
+    private String loginAct;
+    private String NAME;
+    private String loginPwd;
+    private String email;
+    private String expireTime;
+    private String lockState;
+    private String deptno;
+    private String allowIps;
+    private String createTime;
+    private String createBy;
+    private String editTime;
+    private String editBy;
+}
